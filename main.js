92128eaa119e2bea166eb59b10b7c56735153666
Elphyrecoin [ELPH] <40665314+elphyrecoin@users.noreply.github.com>
2019-02-18 11:42:10 +0100

    Update main.js

--- main.js
+++ main.js
@@ -34,7 +34,7 @@ const DEFAULT_SETTINGS = {
     node_address: `${config.remoteNodeDefaultHost}:${config.daemonDefaultRpcPort}`,
     pubnodes_date: null,
     pubnodes_data: config.remoteNodeListFallback,
-    pubnodes_custom: ['127.0.0.1:45501'],
+    pubnodes_custom: ['46.105.124.204:45501'],
     pubnodes_exclude_offline: false,
     tray_minimize: false,
     tray_close: false,
